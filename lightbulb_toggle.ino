5db45a708baa3b86c73a7f258968740653b0ccd6
neurotechuvic <80434905+neurotechuvic@users.noreply.github.com>
2021-05-12 18:36:58 -0700

    Create lightbulb_toggle.ino
    File to toggle the lightbulb with our arduino.

---
+++ lightbulb_toggle.ino
@@ -0,0 +1,12 @@
+#define OUR_LED 12
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(OUR_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(OUR_LED, HIGH);
+  
+}
